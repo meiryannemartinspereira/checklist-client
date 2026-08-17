c6f9e28f95c38a78bb8a45b8a22ab48ae6bdf153
Meiryanne Martins <meirympereira@gmail.com>
2026-08-17 17:03:47 -0300

    refactor: restructure application architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppRoutes from "./routes/appRoutes"
 
-import Dashboard from "./pages/Dashboard";
-import CadastroAluno from "./pages/CadastroAluno";
-import AdminAlunos from "./pages/admin/AdminAlunos";
-import AdminCursos from "./pages/admin/AdminCursos";
-
-
-export default function App() {
-
-    return (
-        <BrowserRouter>
-
-            <Routes>
-
-                <Route 
-                    path="/"
-                    element={<Dashboard />}
-                />
-
-                <Route 
-                    path="/cadastro-aluno"
-                    element={<CadastroAluno />}
-                />
-
-                <Route
-                    path="/admin/alunos"
-                    element={<AdminAlunos />}
-                />
-
-                <Route
-                    path="/admin/cursos"
-                    element={<AdminCursos />}
-                />
-
-            </Routes>
-
-        </BrowserRouter>
-    );
-
+function App() {
+    return <AppRoutes />;
 }
+
+export default App;
--- src/pages/admin/AdminCursos.jsx
+++
@@ -1,8 +0,0 @@
-export default function AdminCursos() {
-    return (
-        <div>
-            <h1>Administração de Cursos</h1>
-            <p>Esta é a página de administração de cursos.</p>
-        </div>
-    );
-}
