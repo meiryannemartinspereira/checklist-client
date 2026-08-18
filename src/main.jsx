9f7aeaeb0473ad8fb5d23ccf0f9f4162c4c210d1
Meiryanne Martins <meirympereira@gmail.com>
2026-08-17 21:08:15 -0300

    feat: add home page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import App from "./App";
 import "./styles/global.css";
 import "./styles/auth.css";
 import "./styles/students.css";
+import "./styles/layout.css";
+import "./styles/home.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -1,14 +1,26 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import {
+    Navigate,
+    Route,
+    Routes
+} from "react-router-dom";
 
 import AuthLayout from "../layouts/AuthLayout";
-import StudentCreate from "../pages/students/StudentCreate";
+import AppLayout from "../layouts/AppLayout";
 
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 
+import Home from "../pages/home/Home";
+
+import StudentCreate from "../pages/students/StudentCreate";
+
 function AppRoutes() {
     return (
         <Routes>
+
+            {/* =========================
+                AUTH
+            ========================= */}
 
             <Route element={<AuthLayout />}>
 
@@ -24,14 +36,48 @@ function AppRoutes() {
 
             </Route>
 
+
+            {/* =========================
+                APPLICATION
+            ========================= */}
+
+            <Route element={<AppLayout />}>
+
+                <Route
+                    path="/home"
+                    element={<Home />}
+                />
+
+                <Route
+                    path="/students/create"
+                    element={<StudentCreate />}
+                />
+
+            </Route>
+
+
+            {/* =========================
+                DEFAULT
+            ========================= */}
+
             <Route
-                path="*"
-                element={<Navigate to="/login" replace />}
+                path="/"
+                element={
+                    <Navigate
+                        to="/home"
+                        replace
+                    />
+                }
             />
 
             <Route
-                path="/students/create"
-                element={<StudentCreate />}
+                path="*"
+                element={
+                    <Navigate
+                        to="/home"
+                        replace
+                    />
+                }
             />
 
         </Routes>
